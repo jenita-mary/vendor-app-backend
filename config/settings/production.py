dfce5fdfc8bc692479fe654a3e25bb2a54b3914d
Jenita Mary <jenitamary1991@gmail.com>
2026-07-03 19:48:25 +0530

    production settings changed

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,3 +7,10 @@ ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
     cast=lambda value: [host.strip() for host in value.split(",")],
 )
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+CSRF_TRUSTED_ORIGINS = config(
+    "CSRF_TRUSTED_ORIGINS",
+    cast=lambda value: [origin.strip() for origin in value.split(",")],
+)
